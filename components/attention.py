a0314fac94b90baaa9a6b35616c3a0f2e0a71b0c
jdvin <josh.daniel.vinson@gmail.com>
2022-07-31 21:03:53 +1000

    Actually implement MHA

--- components/attention.py
+++ components/attention.py
@@ -1,29 +1,7 @@
+import typing as _t
+
 import torch
-import math
-
-
-class SelfAttention(torch.nn.Module):
-    def __init__(self, d_model, d_k, d_v):
-        super().__init__()
-
-        self.d_k = d_k
-        self.q_linear = torch.nn.Linear(d_model, d_k)
-        self.k_linear = torch.nn.Linear(d_model, d_k)
-        self.v_linear = torch.nn.Linear(d_model, d_v)
-
-    def forward(self, q, k, v):
-
-        # batch_size = q.size(0)
-
-        q = self.q_linear(q)  # .view(batch_size, -1, self.d_k)
-        k = self.k_linear(k)
-        v = self.v_linear(v)
-
-        energies = torch.matmul(self.q, k.transpose(-2, -1)) / math.sqrt(self.d_k)
-        self.attention_weights = torch.nn.functional.softmax(energies, -1)
-        output = torch.matmul(self.attention_weights, v)
-
-        return output
+import numpy as np
 
 
 class MultiHeadAttention(torch.nn.Module):
@@ -34,15 +12,19 @@ class MultiHeadAttention(torch.nn.Module):
 
         self.d_model = d_model
         self.h = n_heads
+        self.d_k = d_k
+        self.d_v = d_v
 
-        self.heads = torch.nn.ModuleList(
-            [SelfAttention(d_model=d_model, d_k=d_k, d_v=d_v) for _ in n_heads]
-        )
+        self.W_Q = torch.nn.Parameter(torch.empty((n_heads, d_model, d_k)))
+        self.W_K = torch.nn.Parameter(torch.empty((n_heads, d_model, d_k)))
+        self.W_V = torch.nn.Parameter(torch.empty((n_heads, d_model, d_k)))
+        self.W_O = torch.nn.Parameter(torch.empty((n_heads * d_v, d_model)))
 
         self.dropout = torch.nn.Dropout(dropout)
-        self.out = torch.nn.Linear(d_model, d_model)
 
-    def forward(self, q: torch.Tensor, k, v):
+    def forward(
+        self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor
+    ) -> _t.Tuple[torch.tensor]:
         """Forward pass on all attention heads.
 
         Args:
@@ -54,22 +36,38 @@ class MultiHeadAttention(torch.nn.Module):
             b = Batch size.
             t = Sequence length.
             d = Model depth.
-
-        Returns:
-            _type_: _description_
         """
 
-        # Nest the dimension [1] of the q, k, and v tensors (resulting: b x 1 x t x d tensor) and then repeat them for each head (resulting: b x h x t d tensor).
+        # Get the batch size from the input.
+        b = q.shape[0]
+
+        # Nest the dimension [1] of the q, k, and v tensors (resulting: b x 1 x t x d tensor)
+        # and then repeat them for each head (resulting: b x h x t x d tensor).
         Q = torch.unsqueeze(q, 1).repeat(1, self.h, 1, 1)
         K = torch.unsqueeze(k, 1).repeat(1, self.h, 1, 1)
         V = torch.unsqueeze(v, 1).repeat(1, self.h, 1, 1)
 
-        # Linearly transform each tensor.
+        # Linearly transform each tensor query and key tensor -> (b x h x t x d_k).
+        Q = torch.matmul(Q, self.W_Q)
+        K = torch.matmul(K, self.W_K)
+
+        # Linearly transform each value tensor -> (b x h x t x d_v).
+        V = torch.matmul(V, self.W_V)
 
         # Calculate dot-product self attention for each head.
+        energies = torch.divide(
+            torch.matmul(Q, K.transpose(-1, -2)),
+            np.sqrt(self.d_k),
+        )  # b x h x t x t
 
-        # Concatenate heads and put through final linear layer
-        # concat = scores.transpose(1, 2).contiguous().view(bs, -1, self.d_model)
-        # output = self.out(concat)
+        attention_weights = torch.nn.functional.softmax(energies, -1)  # b x h x t x d_k
+        scores = torch.matmul(attention_weights, V)  # b x h x t x d_v
 
-        # return output
+        # Transpose and concatenate to get a (b x h*d_v x t) tensor.
+        concat = scores.transpose(1, 2).contiguous().view(b, -1, self.h * self.d_v)
+
+        # Run the output through the final linear transformation.
+        output = torch.matmul(concat, self.W_O)
+
+        # Return output and attention weights.
+        return output, attention_weights
