21e56ec22e7c5242c2cbdc9fb8e2d97a5e9853ac
jdvin <josh.daniel.vinson@gmail.com>
2023-06-07 20:56:28 +1000

    Tested and fixed extract function

--- extract_things_data.py
+++ extract_things_data.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 import os
+from typing import Dict, List
 
 import mne
 import polars as pl
@@ -12,6 +13,13 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("participant", type=str, default="sub-02")
 parser.add_argument("--root_path", type=str, default="/Volumes/T7/datasets/things-eeg/")
+
+
+def flatten_epoch_dict(
+    epoch_dict: Dict[str, Dict[int, float]]
+) -> Dict[str, List[float]]:
+    """Dicts with non-string keys cannot be placed into rust structs."""
+    return {key: list(value.values()) for key, value in epoch_dict.items()}
 
 
 def extract_from_eeg_file(
@@ -47,21 +55,26 @@ def extract_from_eeg_file(
             # If the object onset is within 1 sample of the start of the epoch.
             if abs(object_onset - epochs[i].events[0, 0]) <= 1:
                 # Add this and subsequent epoch to the column arrays.
-                object_on_epoch.append(epochs[i].to_data_frame().to_dict())
-                object_off_epoch.append(epochs[i + 1].to_data_frame().to_dict())
+                # TODO: This is a lot of transformations...
+                object_on_epoch.append(
+                    flatten_epoch_dict(epochs[i].to_data_frame().to_dict())
+                )
+                object_off_epoch.append(
+                    flatten_epoch_dict(epochs[i + 1].to_data_frame().to_dict())
+                )
                 # We know the next epoch is not for the next object, so we skip it.
                 i += 2
                 break
             i += 1
+
     object_events_df = object_events_df.with_columns(
-        pl.Series(name="object_on_epoch", values=object_on_epoch),
-        pl.Series(name="object_off_epoch", values=object_off_epoch),
+        pl.Series(name="object_on_epoch", values=object_on_epoch, dtype=pl.Struct),
+        pl.Series(name="object_off_epoch", values=object_off_epoch, dtype=pl.Struct),
     )
 
     object_events_df.write_json(
-        file=os.path.join(root_path, "combined", f"{participant}.json")
+        file=os.path.join(root_path, "combined", f"{participant}.json"), pretty=True
     )
-
     return object_events_df
 
 
