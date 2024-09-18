Fixed bug in causal mask and relative pos

src/components/attention.py
@@ -46,11 +46,8 @@ class MultiHeadAttention(torch.nn.Module):
         if self.is_causal:
             bias = bias.masked_fill(
                 torch.triu(torch.ones(target_seq_len, source_seq_len)).bool(),
-                float("-inf"),
+                torch.finfo(bias.dtype).min,
             )
-        print("source:", source_seq_len)
-        print("target:", target_seq_len)
-        print("bias:", bias.shape)
         self.register_buffer(
             "bias", bias.expand(1, self.n_heads, target_seq_len, source_seq_len)
         )
@@ -74,10 +71,6 @@ class MultiHeadAttention(torch.nn.Module):
             attention_mask: Tensor[float] (B, 1, T_q, T_kv)
         """
         if self.flash:
-            print("q:", q.shape)
-            print("k:", k.shape)
-            print("v:", v.shape)
-            print("mask:", attention_mask.shape)
             y = F.scaled_dot_product_attention(
                 q,
                 k,
@@ -156,5 +149,4 @@ class RelativePositionMultiHeadAttention(MultiHeadAttention):
             self.source_seq_len == self.target_seq_len
         ), "Relative position MHA can only be used in self-attention!"
 
-    def __post_init__(self):
-        self.bias = self.bias + self.rp_bias(self.target_seq_len, self.source_seq_lenr)
+        self.bias = self.bias + self.rp_bias(self.target_seq_len, self.source_seq_len)

src/components/pos.py
@@ -11,7 +11,7 @@ class RelativePositionBias(nn.Module):
         self.num_buckets = num_buckets
         self.max_distance = max_distance
         self.n_heads = n_heads
-        self.weight = nn.Parameter(torch.randn(self.num_buckets, self.n_heads))
+        self.relative_attention_bias = nn.Embedding(self.num_buckets, self.n_heads)
 
     @staticmethod
     def _relative_position_bucket(

src/telepath.py
@@ -14,7 +14,6 @@ from .components.attention import MultiHeadAttention, RelativePositionMultiHeadA
 from .components.activations import GEGLU
 
 from transformers import (
-    AutoModel,
     AutoTokenizer,
     WhisperModel,
     WhisperConfig,
@@ -74,7 +73,7 @@ DECODER_PARAM_MAP = ParamMap(
         "k": "k_proj",
         "v": "v_proj",
         "o": "out_proj",
-        "relative_attention_bias": "rp_bias",
+        "relative_attention_bias": "rp_bias.relative_attention_bias",
         "0layer_norm": "attn_ln",
         "1EncDecAttention": "cross_attn",
         "1layer_norm": "cross_attn_ln",
@@ -473,6 +472,7 @@ class TextDecoder(nn.Module):
         attention_mask: Tensor | None = None,
         kv_cache: dict[int, Tensor] | None = None,
         inference: bool = False,
+        return_hidden_states: bool = False,
     ) -> Tensor:
         offset = kv_cache[next(iter(kv_cache.keys()))].size(1) if kv_cache else 0
         x = self.embed_tokens(x[:, offset:])
@@ -486,8 +486,9 @@ class TextDecoder(nn.Module):
         x = self.ln_post(x)
         if inference:
             x = x[:, -1, :]
+        if return_hidden_states:
+            return x
         logits = x @ torch.transpose(self.embed_tokens.weight.to(x.dtype), 0, 1)
-
         return logits
 
     @torch.no_grad()

tests.py
@@ -72,17 +72,21 @@ def test_encoder():
 
 
 def test_decoder():
-    global resid
-    global ref_resid
+    global activations
+    global ref_activations
     inputs = torch.tensor([[0, 1], [0, 1], [0, 1]])
     # attention_mask = torch.ones_like(inputs)
-    resid = model.decoder(inputs, activations)  # , attention_mask)
+    resid = model.decoder(
+        inputs, activations, return_hidden_states=True
+    )  # , attention_mask)
     ref_resid = ref_decoder(
         input_ids=inputs,
         encoder_hidden_states=ref_activations.last_hidden_state,
         # encoder_attention_mask=attention_mask,
         output_hidden_states=True,
     )
+    print("out shape", resid.shape)
+    print("ref shape", ref_resid.last_hidden_state.shape)
     print(
         "post decoder rtol:",
         torch.max(get_rtol(resid, ref_resid.last_hidden_state)),