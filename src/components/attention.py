Fixed causal mask error, fixed norm error

src/components/attention.py
@@ -45,7 +45,9 @@ class MultiHeadAttention(torch.nn.Module):
         bias = torch.zeros(target_seq_len, source_seq_len)
         if self.is_causal:
             bias = bias.masked_fill(
-                torch.triu(torch.ones(target_seq_len, source_seq_len)).bool(),
+                torch.triu(
+                    torch.ones(target_seq_len, source_seq_len), diagonal=1
+                ).bool(),
                 torch.finfo(bias.dtype).min,
             )
         self.register_buffer(
@@ -151,8 +153,12 @@ class RelativePositionMultiHeadAttention(MultiHeadAttention):
 
         self.bias = self.bias + self.rp_bias(self.target_seq_len, self.source_seq_len)
 
-    def qkv_attention(
-        self, q: Tensor, k: Tensor, v: Tensor, attention_mask: Tensor
-    ) -> Tensor:
-        breakpoint()
-        return super().qkv_attention(q, k, v, attention_mask)
+    def forward(
+        self,
+        x: Tensor,
+        xc: Tensor | None = None,
+        kv_cache: dict[int, Tensor] | None = None,
+        attention_mask: Tensor | None = None,
+    ) -> tuple[Tensor]:
+        # breakpoint()
+        return super().forward(x, xc, kv_cache, attention_mask)

src/telepath.py
@@ -10,6 +10,8 @@ from torch.optim import AdamW
 from torch.optim.lr_scheduler import CosineAnnealingLR, LinearLR, SequentialLR
 import yaml
 
+from src.components.norm import RMSNorm
+
 from .components.attention import MultiHeadAttention, RelativePositionMultiHeadAttention
 from .components.activations import GEGLU
 
@@ -266,9 +268,10 @@ class RelativePositionResidualAttentionBlock(ResidualAttentionBlock):
             dropout=0.1,
             is_causal=is_causal,
         )
+        self.attn_ln = RMSNorm(d_model)
 
-        self.cross_attn = (
-            MultiHeadAttention(
+        if cross_attn:
+            self.cross_attn = MultiHeadAttention(
                 n_heads,
                 d_model,
                 source_seq_len=source_seq_len,
@@ -279,9 +282,9 @@ class RelativePositionResidualAttentionBlock(ResidualAttentionBlock):
                 v_bias=False,
                 dropout=dropout,
             )
-            if cross_attn
-            else None
-        )
+
+            self.cross_attn_ln = RMSNorm(d_model)
+
         self.mlp = nn.Sequential(
             GEGLU(d_model, d_mlp),
             nn.Linear(d_mlp, d_model),